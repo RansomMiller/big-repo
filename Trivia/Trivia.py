dcef4b16d3b9622253bdf5e1794ee50fb5d05d4b
RansomMiller <32290889+RansomMiller@users.noreply.github.com>
2018-05-14 10:38:48 -0700

    CSV file
    Slapped in that good ol csv file and began the imports n crap for it

--- Trivia/Trivia.py
+++ Trivia/Trivia.py
@@ -3,6 +3,9 @@ import mcpi.block as block
 import mcpi.vec3 as Vec3
 import time
 import random
+import csv
 
 mc = minecraft.Minecraft.create()
 
+def Question():
+    open("JEOPARDY_CSV.csv", "r")
