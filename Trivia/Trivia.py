e0429fd307687163422c4189d02a1ce71473e7c5
Minago <38890097+Minago@users.noreply.github.com>
2018-05-17 10:32:00 -0700

    random number generator
    Created the random number generator. Had to change the import of random because it didn't work for some reason. This didn't take 1.5 hours ransom

--- Trivia/Trivia.py
+++ Trivia/Trivia.py
@@ -2,10 +2,29 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 import mcpi.vec3 as Vec3
 import time
-import random
+from random import *
 import csv
 
 mc = minecraft.Minecraft.create()
 
 def Question():
     open("JEOPARDY_CSV.csv", "r")
+
+
+
+
+
+
+
+
+
+
+
+
+
+##Rhys's random number generator
+
+
+random_question = randint (1 , 216930)
+print(random_question)
+
