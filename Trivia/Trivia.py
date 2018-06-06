76f2e7831f0362122d4a851f9bb2e8b709fccf74
Mark S <38958165+marksummer@users.noreply.github.com>
2018-06-06 09:50:48 -0700

    changed emerald to beacon
    player spawns in middle of room, not inside wall

--- Trivia/Trivia.py
+++ Trivia/Trivia.py
@@ -80,42 +80,48 @@ spawn_answer()
 
 
 def location():
-    SIZE = 30
-    pos = mc.player.getTilePos()
-    x = pos.x
-    y = pos.y
-    z = pos.z
-    midx = x + SIZE/2
-    midy = y + SIZE/2
-    mc.setBlocks(x, y, z, x+SIZE+4, y+SIZE, z+SIZE, block.EMERALD_BLOCK.id)
-    mc.setBlocks(x, y, z, x+SIZE/2, y+10, z+SIZE/2, block.GLASS.id)
-    mc.setBlocks(x+1, y, z+1, x+SIZE-2, y+SIZE-1, z+SIZE-2, block.AIR.id)
+    def mainHouse():
+        SIZE = 30
+        pos = mc.player.getTilePos()
+        x = pos.x
+        y = pos.y
+        z = pos.z
+        midx = x + SIZE / 2
+        midy = y + SIZE / 2
+        mc.setBlocks(x - 10, y - 1, z - 10, x + SIZE + 4, y + SIZE, z + SIZE, block.BEACON.id)
+        mc.setBlocks(x - 10, y, z - 10, x + SIZE / 2, y + 10, z + SIZE / 2, block.GLASS.id)
+        mc.setBlocks(x - 9, y, z - 9, x + SIZE - 2, y + SIZE - 1, z + SIZE - 2, block.AIR.id)
 
-def glasshouse():
-    SIZE = 10
-    pos = mc.player.getTilePos()
-    x = pos.x + 5
-    y = pos.y
-    z = pos.z
-    midx = x + SIZE / 2
-    mc.setBlocks(x, y, z, x + SIZE, y + SIZE, z + SIZE, block.GLASS.id)
-    mc.setBlocks(x + 1, y, z + 1, x + SIZE - 2, y + SIZE - 1, z + SIZE - 2, block.AIR.id)
-    mc.setBlocks(midx - 1, y, z, midx + 1, y + 3, z, block.AIR.id)
-    mc.setBlocks(x + 3, y + SIZE - 3, z, midx - 3, z, block.GLASS.id)
-    mc.setBlocks(midx + 3, y + SIZE - 3, z, x + SIZE, z, block.GLASS.id)
-    mc.setBlocks(x, y + SIZE - 1, z, x + SIZE, y + SIZE - 1, z + SIZE, block.GLASS.id)
-    mc.setBlocks(x + 1, y - 1, z + 1, x + SIZE - 2, y - 1, z + SIZE - 2, block.GLASS.id, )
-glasshouse()
+    mainHouse()
 
+    def glasshouse():
+        SIZE = 10
+        pos = mc.player.getTilePos()
+        x = pos.x + 5
+        y = pos.y
+        z = pos.z
+        midx = x + SIZE / 2
+        midy = y + SIZE / 2
+        mc.setBlocks(x, y, z, x + SIZE, y + SIZE, z + SIZE, block.GLASS.id)
+        mc.setBlocks(x + 1, y, z + 1, x + SIZE - 2, y + SIZE - 1, z + SIZE - 2, block.AIR.id)
+        mc.setBlocks(midx - 1, y, z, midx + 1, y + 3, z, block.AIR.id)
+        mc.setBlocks(x + 3, y + SIZE - 3, z, midx - 3, z, block.GLASS.id)
+        mc.setBlocks(midx + 3, y + SIZE - 3, z, x + SIZE, z, block.GLASS.id)
+        mc.setBlocks(x, y + SIZE - 1, z, x + SIZE, y + SIZE - 1, z + SIZE, block.GLASS.id)
+        mc.setBlocks(x + 1, y - 1, z + 1, x + SIZE - 2, y - 1, z + SIZE - 2, block.GLASS.id, )
 
-
-
-
-
-
+    glasshouse()
 
 
-#def anitblock_breaker():
+
+
+
+
+
+
+
+
+    #def anitblock_breaker():
     #for r in range(1, 9):  # how big it scans the blocks
         #BlockOrder2 = getBlocks(Vec3)  # scans the block to see if it's in the original grid
         #if not BlockOrder == BlockOrder2:  # if they are not then it places the original grid
